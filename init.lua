06a50a91e8c4e662d6ca0a80401bd12e27d2b61d
IcyLava <icylavah@gmail.com>
2023-01-24 13:31:47 +0200

    Port init module to lua, provide teal definition

---
+++ init.lua
@@ -0,0 +1,19 @@
+local thisModuleName = ...
+
+-- Ensure submodules can just require 'lithium'
+if thisModuleName ~= 'lithium' then
+	package.preload.lithium = function()
+		return require(thisModuleName)
+	end
+end
+
+setmetatable({}, {
+	__index = function(t, modname)
+		local status, result = pcall(require, "lithium." .. modname)
+		if not status then
+			error(result, 2)
+		end
+		t[modname] = result
+		return result
+	end
+})

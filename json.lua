8d4120ff5b97d4ae43fd6efe6e1ee4bf70be7554
IcyLava <icylavah@gmail.com>
2023-01-30 18:51:41 +0200

    Clean up json.lua

--- json.lua
+++ json.lua
@@ -1,217 +1,216 @@
 local comb = require('lithium.comb')
-local literal, pattern, uptoPattern, whitespace, sequence, eof
-literal, pattern, uptoPattern, whitespace, sequence, eof = comb.literal, comb.pattern, comb.uptoPattern, comb.whitespace, comb.sequence, comb.eof
-local concat, insert, imap
-do
-	local _obj_0 = require('lithium.tablex')
-	concat, insert, imap = _obj_0.concat, _obj_0.insert, _obj_0.imap
+local tablex = require('lithium.tablex')
+
+local literal, pattern, uptoPattern, whitespace, sequence, eof = comb.literal, comb.pattern, comb.uptoPattern, comb.whitespace, comb.sequence, comb.eof
+local concat, insert, imap = tablex.concat, tablex.insert, tablex.imap
+
+local json = {}
+
+local maybe_ws = whitespace:maybe():default('')
+local comma = literal(',')
+local comma_ws = comma:surround(maybe_ws)
+
+local fals = literal('false') / function() return false end
+local tru = literal('true') / function() return true end
+local boolean = tru + fals
+boolean = boolean:tag('boolean')
+
+local null = literal('null') / function() return nil end
+null = null:tag('null')
+
+local number = sequence({
+	pattern('%-?%d+'):index('match'),
+	pattern('%.%d+'):index('match'):maybe():default(''),
+	pattern('[Ee][%-%+]?%d+'):index('match'):maybe():default('')
+}) / concat / tonumber
+number = number:tag('number')
+
+local escape = literal('\\') * (pattern('["\\/bfnrt]') + pattern('u%x%x%x%x')):index('match')
+escape = escape / function(result)
+	if 'u' == result:sub(1, 1) then
+		return '?' -- FIXME: decode unicode escape properly
+	end
+	
+	if 'b' == result then
+		return '\b'
+	elseif 'f' == result then
+		return '\f'
+	elseif 'n' == result then
+		return '\n'
+	elseif 'r' == result then
+		return '\r'
+	elseif 't' == result then
+		return '\t'
+	end
+	
+	return result
 end
-do
-	local json = {}
-	local maybe_ws = whitespace:maybe():default('')
-	local comma = literal(',')
-	local comma_ws = comma:surround(maybe_ws)
-	local fals = literal('false') / function()
-		return false
+
+local strin = sequence({
+	literal('"'),
+	(escape + uptoPattern('["\\%c]')) ^ 0 / concat,
+	literal('"')
+}):second()
+strin = strin:tag('string')
+
+local array, object
+local value = comb.proxy(function() return array + object + boolean + null + number + strin end)
+
+array = sequence({
+	literal('['),
+	maybe_ws,
+	(value:delimited(comma_ws)):maybe():default({}),
+	maybe_ws,
+	literal(']')
+}):third()
+array = array:tag('array')
+
+local record = sequence({
+	strin:index('value'),
+	maybe_ws,
+	literal(':'),
+	maybe_ws,
+	value
+}) / function(result)
+	return {
+		key = result[1],
+		value = result[5]
+	}
+end
+
+object = sequence({
+	literal('{'),
+	maybe_ws,
+	(record:delimited(comma_ws)):maybe():default({}),
+	maybe_ws,
+	literal('}')
+}):third()
+object = object:tag('object')
+
+function json.parse(str)
+	local state = {
+		data = str,
+		index = 1
+	}
+	local err
+	state, err = value:surround(maybe_ws, maybe_ws * eof):run(state)
+	if not (state) then
+		return nil, err
 	end
-	local tru = literal('true') / function()
-		return true
-	end
-	local boolean = tru + fals
-	boolean = boolean:tag('boolean')
-	local null = literal('null') / function()
-		return nil
-	end
-	null = null:tag('null')
-	local number = sequence({
-		pattern('%-?%d+'):index('match'),
-		pattern('%.%d+'):index('match'):maybe():default(''),
-		pattern('[Ee][%-%+]?%d+'):index('match'):maybe():default('')
-	}) / concat / tonumber
-	number = number:tag('number')
-	local escape = literal('\\') * (pattern('["\\/bfnrt]') + pattern('u%x%x%x%x')):index('match')
-	escape = escape / function(result)
-		if 'u' == result:sub(1, 1) then
-			return '?'
+	return state.result
+end
+
+local function decodeValue(val)
+	if 'array' == val.tag then
+		return imap(val.value, decodeValue)
+	elseif 'object' == val.tag then
+		local obj = {}
+		for i = 1, #val.value do
+			local rec = val.value[i]
+			obj[rec.key] = decodeValue(rec.value)
 		end
-		local _exp_0 = result
-		if 'b' == _exp_0 then
-			return '\b'
-		elseif 'f' == _exp_0 then
-			return '\f'
-		elseif 'n' == _exp_0 then
-			return '\n'
-		elseif 'r' == _exp_0 then
-			return '\r'
-		elseif 't' == _exp_0 then
-			return '\t'
-		else
-			return result
-		end
+		return obj
+	else
+		return val.value
 	end
-	local strin = sequence({
-		literal('"'),
-		(escape + uptoPattern('["\\%c]')) ^ 0 / concat,
-		literal('"')
-	}):second()
-	strin = strin:tag('string')
-	local array, object
-	local value = comb.proxy(function()
-		return array + object + boolean + null + number + strin
-	end)
-	array = sequence({
-		literal('['),
-		maybe_ws,
-		(value:delimited(comma_ws)):maybe():default({}),
-		maybe_ws,
-		literal(']')
-	}):third()
-	array = array:tag('array')
-	local record = sequence({
-		strin:index('value'),
-		maybe_ws,
-		literal(':'),
-		maybe_ws,
-		value
-	}) / function(result)
-		return {
-			key = result[1],
-			value = result[5]
-		}
+end
+
+function json.decode(str)
+	local result, err = json.parse(str)
+	if not (result) then
+		return nil, err
 	end
-	object = sequence({
-		literal('{'),
-		maybe_ws,
-		(record:delimited(comma_ws)):maybe():default({}),
-		maybe_ws,
-		literal('}')
-	}):third()
-	object = object:tag('object')
-	json.parse = function(str)
-		local state = {
-			data = str,
-			index = 1
-		}
-		local err
-		state, err = value:surround(maybe_ws, maybe_ws * eof):run(state)
-		if not (state) then
-			return nil, err
-		end
-		return state.result
+	return decodeValue(result)
+end
+
+local function ind(level)
+	if level == nil then
+		level = 0
 	end
-	local decodeValue
-	decodeValue = function(val)
-		local _exp_0 = val.tag
-		if 'array' == _exp_0 then
-			return imap(val.value, decodeValue)
-		elseif 'object' == _exp_0 then
-			local obj = {}
-			local _list_0 = val.value
-			for _index_0 = 1, #_list_0 do
-				local rec = _list_0[_index_0]
-				obj[rec.key] = decodeValue(rec.value)
-			end
-			return obj
-		else
-			return val.value
-		end
+	return ('\t'):rep(level)
+end
+
+local function encodeValue(val, indent)
+	if indent == nil then
+		indent = 0
 	end
-	json.decode = function(str)
-		local result, err = json.parse(str)
-		if not (result) then
-			return nil, err
-		end
-		return decodeValue(result)
-	end
-	local ind
-	ind = function(level)
-		if level == nil then
-			level = 0
-		end
-		return ('\t'):rep(level)
-	end
-	local encodeValue
-	encodeValue = function(val, indent)
-		if indent == nil then
-			indent = 0
-		end
-		local _exp_0 = type(val)
-		if 'string' == _exp_0 then
-			val = val:gsub('[%z\1-\31\\"]', function(char)
-				local _exp_1 = char
-				if '\\' == _exp_1 then
-					return '\\\\'
-				elseif '"' == _exp_1 then
-					return '\\"'
-				elseif '\r' == _exp_1 then
-					return '\\r'
-				elseif '\n' == _exp_1 then
-					return '\\n'
-				elseif '\t' == _exp_1 then
-					return '\\t'
-				elseif '\b' == _exp_1 then
-					return '\\b'
-				else
-					return string.format('\\u%04x', char:byte())
-				end
-			end)
-			return '"' .. val .. '"'
-		elseif 'table' == _exp_0 then
-			local maxIndex = 0
-			local isArray = true
-			for k, v in pairs(val) do
-				local kt = type(k)
-				if kt ~= 'number' and kt ~= 'string' then
-					return nil, 'object key may only be a string or a number'
-				end
-				if isArray and kt == 'number' and k >= 1 then
-					if k > maxIndex then
-						maxIndex = k
-					end
-				else
-					isArray = false
-					break
-				end
-			end
-			if isArray then
-				local t = {}
-				for i = 1, maxIndex do
-					local err
-					t[i], err = encodeValue(val[i])
-					if not (t[i]) then
-						return nil, err
-					end
-				end
-				return "[" .. tostring(concat(t, ', ', 1, maxIndex)) .. "]"
+	local typ = type(val)
+	if 'string' == typ then
+		val = val:gsub('[%z\1-\31\\"]', function(char)
+			if '\\' == char then
+				return '\\\\'
+			elseif '"' == char then
+				return '\\"'
+			elseif '\r' == char then
+				return '\\r'
+			elseif '\n' == char then
+				return '\\n'
+			elseif '\t' == char then
+				return '\\t'
+			elseif '\b' == char then
+				return '\\b'
 			else
-				local t = {}
-				for k, v in pairs(val) do
-					k = tostring(k)
-					if t[k] then
-						return nil, 'number - string object key name clash'
-					end
-					t[k] = true
-					local err
-					k, err = encodeValue(k, indent + 1)
-					if not (k) then
-						return nil, err
-					end
-					v, err = encodeValue(v, indent + 1)
-					if not (v) then
-						return nil, err
-					end
-					insert(t, tostring(ind(indent + 1)) .. tostring(k) .. ": " .. tostring(v))
-				end
-				return "{\n" .. tostring(concat(t, ',\n')) .. "\n" .. tostring(ind(indent)) .. "}"
+				return string.format('\\u%04x', char:byte())
 			end
-		elseif 'nil' == _exp_0 then
-			return 'null'
-		else
-			return tostring(val)
+		end)
+		return '"' .. val .. '"'
+	elseif 'table' == typ then
+		local maxIndex = 0
+		local isArray = true
+		for k, _ in pairs(val) do
+			local kt = type(k)
+			if kt ~= 'number' and kt ~= 'string' then
+				return nil, 'object key may only be a string or a number'
+			end
+			if isArray and kt == 'number' and k >= 1 then
+				if k > maxIndex then
+					maxIndex = k
+				end
+			else
+				isArray = false
+				break
+			end
 		end
+		if isArray then
+			local t = {}
+			for i = 1, maxIndex do
+				local err
+				t[i], err = encodeValue(val[i])
+				if not (t[i]) then
+					return nil, err
+				end
+			end
+			return "[" .. tostring(concat(t, ', ', 1, maxIndex)) .. "]"
+		else
+			local t = {}
+			for k, v in pairs(val) do
+				k = tostring(k)
+				if t[k] then
+					return nil, 'number - string object key name clash'
+				end
+				t[k] = true
+				local err
+				k, err = encodeValue(k, indent + 1)
+				if not (k) then
+					return nil, err
+				end
+				v, err = encodeValue(v, indent + 1)
+				if not (v) then
+					return nil, err
+				end
+				insert(t, tostring(ind(indent + 1)) .. tostring(k) .. ": " .. tostring(v))
+			end
+			return "{\n" .. tostring(concat(t, ',\n')) .. "\n" .. tostring(ind(indent)) .. "}"
+		end
+	elseif 'nil' == typ then
+		return 'null'
+	else
+		return tostring(val)
 	end
-	json.encode = function(val)
-		return encodeValue(val)
-	end
-	return json
 end
+
+function json.encode(val)
+	return encodeValue(val)
+end
+
+return json

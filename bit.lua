db743f1835961fa888256946eb25cb9ec4aae594
IcyLava <icylavah@gmail.com>
2023-03-11 23:00:16 +0200

    Inline all the things

--- bit.lua
+++ bit.lua
@@ -6,34 +6,13 @@ local bit = {}
 
 local loadstring = loadstring or load
 local format = string.format
-local floor = math.floor
-local ceil = math.ceil
 
-local function round(x)
-	if x < 0 then
-		return ceil(x - 0.5)
-	else
-		return floor(x + 0.5)
-	end
-end
-
-local function normalize(x)
-	return round(x) % 2 ^ 32
-end
-
-local function normalizeShift(n)
-	return round(n) % 2 ^ 5
-end
-
-local function uint2int(x)
+function bit.tobit(x)
+	x = (x - x % 1) % 2 ^ 32
 	if x >= 2 ^ 31 then
 		return x - 2 ^ 32
 	end
 	return x
-end
-
-function bit.tobit(x)
-	return uint2int(normalize(x))
 end
 
 function bit.tohex(x, n)
@@ -48,7 +27,7 @@ function bit.tohex(x, n)
 		upper = true
 	end
 	
-	x = normalize(x)
+	x = x % 2 ^ 32
 	
 	local str
 	if upper then
@@ -61,16 +40,20 @@ function bit.tohex(x, n)
 end
 
 function bit.bnot(x)
-	return uint2int(normalize(-round(x) - 1))
+	x = (-(x - x % 1) - 1) % 2 ^ 32
+	if x >= 2 ^ 31 then
+		return x - 2 ^ 32
+	end
+	return x
 end
 
 local logicTemplate = [[
 	return function(a, ...)
-		a = (a + 0.5) % 2 ^ 32
+		a = a % 2 ^ 32
 		local result
 		for i = 1, select('#', ...) do
 			result = 0
-			local b = (select(i, ...) + 0.5) % 2 ^ 32
+			local b = select(i, ...) % 2 ^ 32
 			{body}
 			a = result
 		end
@@ -151,57 +134,77 @@ do
 end
 
 function bit.lshift(x, n)
-	n = normalizeShift(n)
-	return uint2int(round(x) % 2 ^ (32 - n) * 2 ^ (n))
+	n = (n - n % 1) % 2 ^ 5
+	x = (x - x % 1) % 2 ^ (32 - n) * 2 ^ n
+	if x >= 2 ^ 31 then
+		x = x - 2 ^ 32
+	end
+	return x
 end
 
 function bit.rshift(x, n)
-	n = normalizeShift(n)
-	x = normalize(x)
-	return uint2int(floor(x / 2 ^ n))
+	n = (n - n % 1) % 2 ^ 5
+	x = x % 2 ^ 32
+	x = x / 2 ^ n
+	x = x - x % 1
+	if x >= 2 ^ 31 then
+		x = x - 2 ^ 32
+	end
+	return x
 end
 
 function bit.arshift(x, n)
-	n = normalizeShift(n)
-	x = normalize(x)
-	local result = floor(x / 2 ^ n)
+	n = (n - n % 1) % 2 ^ 5
+	x = x % 2 ^ 32
+	local result = x / 2 ^ n
+	result = result - result % 1
 	if x >= 2 ^ 31 then
 		result = result + (2 ^ n - 1) * 2 ^ (32 - n)
 	end
-	return uint2int(result)
+	if result >= 2 ^ 31 then
+		result = result - 2 ^ 32
+	end
+	return result
 end
 
 function bit.rol(x, n)
-	n = normalizeShift(n)
-	x = normalize(x)
+	n = (n - n % 1) % 2 ^ 5
+	x = (x - x % 1) % 2 ^ 32
 	local low = x % 2 ^ (32 - n)
-	return uint2int(
-		(x - low) / 2 ^ (32 - n) +
-		low * 2 ^ n
-	)
+	x = (x - low) / 2 ^ (32 - n) + low * 2 ^ n
+	if x >= 2 ^ 31 then
+		x = x - 2 ^ 32
+	end
+	return x
 end
 
 function bit.ror(x, n)
-	n = normalizeShift(n)
-	x = normalize(x)
+	n = (n - n % 1) % 2 ^ 5
+	x = (x - x % 1) % 2 ^ 32
 	local low = x % 2 ^ n
-	return uint2int(
-		(x - low) / 2 ^ n +
-		low * 2 ^ (32 - n)
-	)
+	x = (x - low) / 2 ^ n + low * 2 ^ (32 - n)
+	if x >= 2 ^ 31 then
+		x = x - 2 ^ 32
+	end
+	return x
 end
 
 function bit.bswap(x)
-	x = normalize(x)
+	x = (x - x % 1) % 2 ^ 32
+	
 	local b1 = x % (2 ^ 8)
 	local b2 = x % (2 ^ 16)
 	local b3 = x % (2 ^ 24)
-	return uint2int(
-		b1 * 2 ^ 24 +
+	
+	x = b1 * 2 ^ 24 +
 		(b2 - b1) * 2 ^ 8 +
 		(b3 - b2) / 2 ^ 8 +
 		(x - b3) / 2 ^ 24
-	)
+	
+	if x >= 2 ^ 31 then
+		x = x - 2 ^ 32
+	end
+	return x
 end
 
 return bit
